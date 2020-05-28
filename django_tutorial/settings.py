e5dd3f2cb348e129e2373de7fada406e9d2b9b47
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-05-28 18:07:41 +0300

    Sitemaps.xml added

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -44,6 +44,8 @@ EMAIL_USE_TLS = env("EMAIL_USE_TLS")
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 
 # Application definition
 
@@ -54,6 +56,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
     "blog.apps.BlogConfig",
     "taggit",
 ]

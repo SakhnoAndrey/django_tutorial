957f37b951c72cc8df284515d7dc38bbc2a4ac75
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-05-30 16:15:01 +0300

    SearchQuery and SearchRank

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from taggit.models import Tag
 from django.db.models import Count
 from .models import Post, Comment
@@ -117,9 +117,17 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            results = Post.objects.annotate(
-                search=SearchVector("title", "body")
-            ).filter(search=query)
+            search_vector = SearchVector("title", weight="A") + SearchVector(
+                "body", weight="B"
+            )
+            search_query = SearchQuery(query)
+            results = (
+                Post.objects.annotate(
+                    search=search_vector, rank=SearchRank(search_vector, search_query),
+                )
+                .filter(rank__gte=0.3)
+                .order_by("-rank")
+            )
     return render(
         request,
         "blog/post/search.html",
